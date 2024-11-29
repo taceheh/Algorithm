cc671393e8da182468b0642a1f6c3e949abd5988
wonheeki <105844632+wonheeki@users.noreply.github.com>
2024-11-29 14:58:32 +0900

    [Bronze II] Title: Hashing, Time: 116 ms, Memory: 34892 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,6 +3,7 @@ import string
 
 alphabet = {}
 result=0
+M = 1234567891
 
 for index, char in enumerate(string.ascii_lowercase, start=1):
     alphabet[char] = index
@@ -15,6 +16,6 @@ for i in range(n):
     # print(alphabet[st[i]])
     result+=alphabet[st[i]]*31**i
 
-print(result)
+print(result%M)
 
 

65347054c14a4d1c2650a42015eb9f417a9e3284
wonheeki <105844632+wonheeki@users.noreply.github.com>
2024-11-29 14:48:43 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 34768 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,20 @@
+# Hashing
+import string
+
+alphabet = {}
+result=0
+
+for index, char in enumerate(string.ascii_lowercase, start=1):
+    alphabet[char] = index
+
+
+n = int(input())
+st=list(input())
+
+for i in range(n):
+    # print(alphabet[st[i]])
+    result+=alphabet[st[i]]*31**i
+
+print(result)
+
+

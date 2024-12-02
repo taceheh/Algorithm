143940106dbc290f265905cbe4a26d729d3db1aa
wonheeki <105844632+wonheeki@users.noreply.github.com>
2024-12-02 12:18:33 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -0,0 +1,28 @@
+# Yangjojang of The Year
+import sys
+# 테스트케이스  
+t = int(sys.stdin.readline())
+result=[]
+
+for _ in range(t):
+    # 학교의 숫자 정수 
+    n = int(sys.stdin.readline())
+    # 학교명과 술소비량을 담을 딕셔너리 선언
+    univercity ={}
+    first_uni = ""
+    max_amount=0
+    for _ in range(n):
+        name,amount = sys.stdin.readline().split()
+        if name not in univercity:
+            univercity[name]= int(amount)
+        else :
+            univercity[name]+=int(amount)
+            
+        if int(amount)>max_amount:
+            max_amount=int(amount)
+            first_uni= name
+    result.append(first_uni)
+
+
+for r in result:
+    print(r)
